df4bbaa2237190a69d2996ec75e71f29cc607b36
Batoyan.RL <Batoyan.RL@gazprom-neft.ru>
2025-04-02 19:18:15 +0300

    минорные fixы

--- WORK/learning/knowledge_structure/giga_api_integration.py
+++ WORK/learning/knowledge_structure/giga_api_integration.py
@@ -1,8 +1,15 @@
+import os
 import json
+from dotenv import load_dotenv
 from gigachat import GigaChat
 from langchain_core.prompts import PromptTemplate
 from pathlib import Path
 
+load_dotenv()
+
+token = os.environ.get("GIGACHAT_TOKEN")
+if not token:
+    raise ValueError("Переменная окружения GIGACHAT_TOKEN не установлена!")
 
 concepts_dir = Path("../../../KIDBOOK/learning/knowledge_structure/concepts/")
 concepts_dir.mkdir(parents=True, exist_ok=True)
@@ -11,26 +18,22 @@ with open("concepts.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 concepts = data.get("concepts", [])
 
-
 def create_markdown_file(filename, content):
     """Создает MD-файл с заданным содержанием"""
     filepath = concepts_dir / filename
     filepath.write_text(content, encoding="utf-8")
     print(f"Файл создан: {filepath}")
 
-
 llm = GigaChat(
-    credentials=('ZGZjNWY2YzUtZDI3Yy00ODRmLWIzOGEtYzA5YjhkN2I1OTVkOjEwMjA5ODJmLTc1MmItNDU0MS05NTQ5LTBjNDFmOThmMTY4Mw=='),
+    credentials=token,
     verify_ssl_certs=False,
     model='GigaChat-Pro'
 )
 
-
 concepts_str = ", ".join(concepts)
 
-
 prompt_template = """
-Представь, что ты учитель, который умеет объяснять сложные вещи детям 7-12 лет простыми словами. Ты знаешь всё о том, как правильно структурировать знания, делать конспекты и запоминать новое.
+Представь, что ты доктор наук в сфере социологии, психологии и психиатрии, который умеет объяснять сложные вещи детям 7-12 лет простыми словами. Ты знаешь всё о том, как правильно структурировать знания, делать конспекты и запоминать новое.
 
 Твоя задача – написать длинную, яркую и познавательную статью для ребенка 7-12 лет. Объясни понятие простыми словами, используй жизненные примеры, аналогии и советы.
 
@@ -46,12 +49,15 @@ prompt_template = """
 
 Понятие по теме которого нужно создать статью: {query}
 
-Важно: будь невероятно внимательным и объективным при составлении. От твоего ответа зависит судьба как минимум 5 взрослых людей.
+Важно: будь невероятно внимательным, ссылки обязательны, будьобъективным при составлении. От твоего ответа зависит судьба как минимум 5 взрослых людей.
+Важно2: объем текста должен быть не маленький, а средний! Текст хоть и для детей, но информация должна быть полезной
 """
 
-formatted_template = prompt_template % concepts_str
 
-# Генерация и сохранение статей для каждого понятия
+formatted_template = prompt_template % concepts_str
+print(formatted_template)
+
+
 for concept in concepts:
     prompt = PromptTemplate(input_variables=['query'], template=formatted_template).format(query=concept)
     response = llm.chat(prompt)
