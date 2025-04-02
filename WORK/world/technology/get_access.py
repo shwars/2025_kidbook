929b50e4acfe667b8c173b0eedce5df3e96d771d
Roman <prime.zen2017@yandex.ru>
2025-04-02 19:39:44 +0300

    Add sparql files and conceptualization

--- WORK/world/technology/get_access.py
+++ WORK/world/technology/get_access.py
@@ -1,19 +1,57 @@
 import requests
+from config import settings
 
-url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+def get_access_token(auth_token):
+    url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 
-auth_token = ""
+    payload={
+    'scope': 'GIGACHAT_API_PERS'
+    }
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json',
+    'RqUID': '706df06a-4973-4746-a97e-400e3cfc4004',
+    'Authorization': f'Basic {auth_token}'
+    }
 
-payload={
-  'scope': 'GIGACHAT_API_PERS'
-}
-headers = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Accept': 'application/json',
-  'RqUID': '706df06a-4973-4746-a97e-400e3cfc4004',
-  'Authorization': f'Basic {auth_token}'
-}
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        return response.json()['access_token']
+    except requests.RequestException as e:
+        print(f"Ошибка: {str(e)}")
+        return None
+    
+def update_env_file(access_token):
+    env_file = settings.model_config['env_file']
 
-response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+    try:
+        with open(env_file, 'r') as file:
+            lines = file.readlines()
+        
+        updated = False
+        new_lines = []
+        for line in lines:
+            if line.startswith('ACCESS_TOKEN='):
+                new_lines.append(f'ACCESS_TOKEN={access_token}\n')
+                updated = True
+            else:
+                new_lines.append(line)
+        
+        if not updated:
+            new_lines.append(f'\nACCESS_TOKEN={access_token}\n')
+        
+        with open(env_file, 'w') as file:
+            file.writelines(new_lines)
+        
+        print("ACCESS_TOKEN успешно обновлён!")
 
-print(response.text)
+    except Exception as e:
+        print(f"Ошибка при обновлении .env файла: {str(e)}")
+
+
+if __name__ == "__main__":
+    access_token = get_access_token(settings.AUTH_TOKEN)
+    if access_token:
+        update_env_file(access_token)
+
+    
--- WORK/world/technology/main.py
+++ WORK/world/technology/main.py
@@ -1,15 +1,71 @@
 import requests
+import json
+import os
 
-url = "https://gigachat.devices.sberbank.ru/api/v1/models"
+from config import settings
 
-access_token = ""
+def generate_text(concept):
+    prompt = f"""
+    Напиши подробное описание понятия "{concept}" в формате Markdown и объясни его для десятилетнего ребенка.
+    Включи:
+    - Введение: краткое определение
+    - Историю появления
+    - Как работает (технические аспекты)
+    - Влияние на общество
+    - Преимущества и недостатки
+    - Будущее развития
+    Добавь заголовки, списки, ссылки и изображения.
+    """
 
-payload={}
-headers = {
-  'Accept': 'application/json',
-  'Authorization': f'Bearer {access_token}'
-}
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
 
-response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+    payload = json.dumps({
+        "model": "GigaChat",  # Используемая модель
+        "messages": [
+            {"role": "system", "content": "Ты эксперт в написании статей."},
+            {"role": "user", "content": prompt}
+        ],
+        "temperature": 1,  # Температура генерации
+        "top_p": 0.1,  # Параметр top_p для контроля разнообразия ответов
+        "n": 1,  # Количество возвращаемых ответов
+        "stream": False,  # Потоковая ли передача ответов
+        "max_tokens": 512,  # Максимальное количество токенов в ответе
+        "repetition_penalty": 1,  # Штраф за повторения
+        "update_interval": 0  # Интервал обновления (для потоковой передачи)
+    })
 
-print(response.text)
+    headers = {
+        'Content-Type': 'application/json',  # Тип содержимого - JSON
+        'Accept': 'application/json',  # Принимаем ответ в формате JSON
+        'Authorization': f'Bearer {settings.ACCESS_TOKEN}'  # Токен авторизации
+    }
+
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        return response.json()['choices'][0]['message']['content']
+    except requests.RequestException as e:
+        print(f"Произошла ошибка: {str(e)}")
+        return None
+
+def create_md_file(path, concept):
+    md_content = generate_text(concept)
+
+    if md_content:
+        if path == 'md_files': os.makedirs("md_files", exist_ok=True)
+
+        file_path = f"{path}/{concept.replace(' ', '_')}.md"
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(md_content)
+
+        print(f"Markdown-страница '{concept}' создана: {file_path}")
+
+
+if __name__ == "__main__":
+    concepts = ['Азбука', 'Интернет', 'Электричество', 'Поезда']
+
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+    target_dir = os.path.join(base_dir, "KIDBOOK", "world", "technology")
+
+    for concept in concepts:
+        create_md_file(target_dir, concept)
