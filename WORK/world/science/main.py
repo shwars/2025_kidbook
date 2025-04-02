9f1711cc8229290697dbc81032e5fa95d2c0d552
JonAJ <jonajmail@gmail.com>
2025-04-02 21:18:35 +0300

    Generated md files

--- WORK/world/science/main.py
+++ WORK/world/science/main.py
@@ -1,18 +1,30 @@
 from modules.config import settings
 from modules.explanation import explain_for_kids
 from modules.create_md import create_md_file, is_md_file_exists, get_files_to_create
+from modules.links import science_terms
+from modules.wiki_content import get_concise_info
 
 
 
 def main():
-    files_to_create = get_files_to_create()
     
-    for filename in files_to_create:
-        if not is_md_file_exists(settings.kidbook_path + '/world/science', filename):
-            create_md_file(settings.kidbook_path + '/world/science', filename, filename)
-            print("Created file:", filename + ".md")
+    for term in science_terms:
+        wiki_info = get_concise_info(term)
+        info_for_kids = explain_for_kids(text=wiki_info)
+        
+        if not is_md_file_exists(settings.kidbook_path + '/world/science', term.lower().replace(' ', '_') + '.md'):
+            create_md_file(settings.kidbook_path + '/world/science', term.lower().replace(' ', '_') + '.md', info_for_kids)
+            print("Created file:", term.lower().replace(' ', '-') + ".md")
         else:
-            print("File already exists:", filename + ".md")
+            print("File already exists:", term.lower().replace(' ', '-') + ".md")
+        
+        
+        
+        print(info_for_kids)
+        print("=========================================================")  
+    
+    
+    
             
                  
             
